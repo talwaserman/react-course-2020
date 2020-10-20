db04e6ea4f4306c06240fb7f18775afc101847f0
Tal Waserman <twaserman@ebay.com>
2020-10-20 21:45:31 +0300

    add support for the react course labs

---
+++ lab11_1/src/main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { useState } from 'react';
+
+const App = () => {
+
+  return (
+    <div>
+      <p>The largest counter is: ... </p>
+    </div>
+  )
+};
+
+
+// main.js
+const root = document.querySelector('main');
+ReactDOM.render(<App />, root);
